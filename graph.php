848fef2bc8056ad5b7401a78187979284e09e916
Simon Garny <simon.garny@prestashop.com>
2019-05-21 09:34:56 +0200

    first commit for the graph file

---
+++ graph.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<html>
+<head>
+    <title>Stats</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+<body>
+<div class="navbar">
+    <div class="navbar_container">
+        <div class="title">
+            <h2>Graph reporting</h2>
+        </div>
+
+    </div>
+</div>
+<div class="container">
+    <div class="details">
+        <div class="options">
+            <div class="blocks_container">
+                <button id="fold"><i class="material-icons">unfold_more</i>Unfold everything</button>
+            </div>
+        </div>
+
+
+
+        TOTO
+    </div>
+</div>
+</body>
+</html>
