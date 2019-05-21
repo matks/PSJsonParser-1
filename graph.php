8a97a1d138ca74c9f4155ee6e7d4db3758663c71
Simon Garny <simon.garny@prestashop.com>
2019-05-21 17:00:19 +0200

    creating the form

--- graph.php
+++ graph.php
@@ -5,7 +5,37 @@ require_once('config.php');
 $start_date = date('Y-m-d', strtotime('-2 weeks'));
 $end_date = date('Y-m-d');
 
-$version = '';
+$selected_version = '';
+$selected_campaign = '';
+
+//db values
+$execution = new Execution($db);
+//versions
+$versions = $execution->getVersions();
+//campaigns
+$suite = new Suite($db);
+$campaigns = $suite->getCampaigns();
+
+//merge with data from the GET array
+$get = $_GET;
+if (isset($get) && sizeof($get) > 0) {
+    if (isset($get['start_date']) && $get['start_date'] != '') {
+        $start_date = $get['start_date'];
+    }
+    if (isset($get['end_date']) && $get['end_date'] != '') {
+        $end_date = $get['end_date'];
+    }
+    if (isset($get['version']) && $get['version'] != '') {
+        $selected_version = $get['version'];
+    }
+    if (isset($get['campaign']) && $get['campaign'] != '') {
+        $selected_campaign = $get['campaign'];
+    }
+}
+
+//get the data
+
+
 ?>
 <html>
 <head>
@@ -22,38 +52,53 @@ $version = '';
         <div class="title">
             <h2>Graph reporting</h2>
         </div>
-
     </div>
 </div>
 <div class="container">
     <div class="details">
         <div class="options">
             <div class="blocks_container">
-                <form action="" method="GET" id="displayform">
+                <form action="" method="GET" id="graphform">
                     <div class="form_block">
-                        <label for="start_date">
-                            Start date
-                            <input type="date" name="start_date" id="start_date" value="<?php echo $start_date; ?>"/>
-                        </label>
-                    </div>
-                    <div class="form_block">
-                        <label for="end_date">
-                            End date
-                            <input type="date" name="end_date" id="end_date" value="<?php echo $end_date; ?>"/>
-                        </label>
-                    </div>
-                    <div class="form_block">
+                        <div class="form_container">
+                            <label for="start_date">
+                                Start date
+                                <input type="date" name="start_date" id="start_date" value="<?php echo $start_date; ?>" max="<?php echo date('Y-m-d'); ?>"/>
+                            </label>
+                        </div>
+                        <div class="form_container">
+                            <label for="end_date">
+                                End date
+                                <input type="date" name="end_date" id="end_date" value="<?php echo $end_date; ?>" max="<?php echo date('Y-m-d'); ?>"/>
+                            </label>
+                        </div>
+                    </div><div class="form_block">
                         <label for="version">
                             Version
                             <select name="version" id="version">
                                 <?php
-                                    foreach($version as $v) {
-                                        $s = ($version == $v->version) ? 'selected' : '';
+                                    foreach($versions as $v) {
+                                        $s = ($selected_version == $v->version) ? 'selected' : '';
                                         echo '<option value="'.$v->version.'" '.$s.'>'.$v->version.'</option>';
                                     }
                                 ?>
                             </select>
                         </label>
+                    </div><div class="form_block">
+                        <label for="Campaign">
+                            Campaign
+                            <select name="campaign" id="campaign">
+                                <option value="">All</option>
+                                <?php
+                                    foreach($campaigns as $c) {
+                                        $s = ($selected_campaign == $c->campaign) ? 'selected' : '';
+                                        echo '<option value="'.$c->campaign.'" '.$s.'>'.$c->campaign.'</option>';
+                                    }
+                                ?>
+                            </select>
+                        </label>
+                    </div><div class="form_block">
+                        <button>Filter</button>
                     </div>
                 </form>
             </div>
