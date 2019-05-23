14ada80f43f9998cb166d0d3111c8fa1f26c78ec
Simon Garny <simon.garny@prestashop.com>
2019-05-23 16:45:27 +0200

    modify the logger class to handle a missing log folder

--- class/Logger.php
+++ class/Logger.php
@@ -5,6 +5,13 @@ class Logger
     static $log_file = "log/log.log";
 
     static function log($log, $level='log') {
+
+        $dirname = dirname(self::$log_file);
+        if (!is_dir($dirname))
+        {
+            mkdir($dirname, 0755, true);
+        }
+
         if($log != '') {
             $fh = fopen(self::$log_file, "a");
             fwrite($fh,date('Y-m-d H:i:s')."\t[$level] $log\n");
